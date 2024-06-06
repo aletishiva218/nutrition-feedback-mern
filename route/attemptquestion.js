0157642ce3d622c12f6301e5944d512ebe1137d8
Shiva Aleti <aletishiva218@gmail.com>
2024-06-06 21:18:13 +0530

    updated put api

--- route/attemptquestion.js
+++ route/attemptquestion.js
@@ -8,22 +8,37 @@ const attemptQuestion = async (req,res) => {
          return res.status(404).json({status:false,message:"user does not exists"})
     const attemptQuestion={
         nutrition:{
-            carbohydrates:(carbohydrates!=undefined)?carbohydrates:null,
-            proteins:(proteins!=undefined)?proteins:null,
-            fats:(fats!=undefined)?fats:null,
-            calcium:(calcium!=undefined)?calcium:null,
-            fiber:(fiber!=undefined)?fiber:null,
-            sodium_chloride:(sodium_chloride!=undefined)?sodium_chloride:null,
-            sucralose:(sucralose!=undefined)?sucralose:null,
-            sucrose:(sucrose!=undefined)?sucrose:null,
-            acesulfame_potassium:(acesulfame_potassium!=undefined)?acesulfame_potassium:null,
-            monosodium_glutamate:(monosodium_glutamate!=undefined)?monosodium_glutamate:null,
-            corn_syrup:(corn_syrup!=undefined)?corn_syrup:null,
-            dextrose:(dextrose!=undefined)?dextrose:null,
-            mannitol:(mannitol!=undefined)?mannitol:null,
-            honey:(honey!=undefined)?honey:null
         }
     }
+
+    if(carbohydrates!=undefined)
+        attemptQuestion.nutrition.carbohydrates=carbohydrates;
+    if(proteins!=undefined)
+        attemptQuestion.nutrition.proteins=proteins;
+    if(fats!=undefined)
+        attemptQuestion.nutrition.fats=fats;
+    if(calcium!=undefined)
+        attemptQuestion.nutrition.calcium=calcium;
+    if(fiber!=undefined)
+        attemptQuestion.nutrition.fiber=fiber;
+    if(sodium_chloride!=undefined)
+        attemptQuestion.nutrition.sodium_chloride=sodium_chloride;
+    if(sucralose!=undefined)
+        attemptQuestion.nutrition.sucralose=sucralose;
+    if(sucrose!=undefined)
+        attemptQuestion.nutrition.sucrose=sucrose;
+    if(acesulfame_potassium!=undefined)
+        attemptQuestion.nutrition.acesulfame_potassium=acesulfame_potassium;
+    if(monosodium_glutamate!=undefined)
+        attemptQuestion.nutrition.monosodium_glutamate=monosodium_glutamate;
+    if(corn_syrup!=undefined)
+        attemptQuestion.nutrition.corn_syrup=corn_syrup;
+    if(dextrose!=undefined)
+        attemptQuestion.nutrition.dextrose=dextrose;
+    if(mannitol!=undefined)
+        attemptQuestion.nutrition.mannitol=mannitol;
+    if(honey!=undefined)
+        attemptQuestion.nutrition.honey=honey;
     
     if(req.body.opinion)
         {
